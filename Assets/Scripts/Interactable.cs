23a7c83917fe391732bdb4045fe6234fb3d5072e
AndreiMoraru123 <81184255+AndreiMoraru123@users.noreply.github.com>
2024-12-18 08:51:12 +0200

    interaction events

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public abstract class Interactable : MonoBehaviour
 {
+    public bool useEvents;
+
+    [SerializeField]
     public string promptMessage;
     public void BaseInteract()
     {
+        if (useEvents)
+        {
+
+            GetComponent<InteractionEvent>().onInteract.Invoke();
+        }
         Interact();
     }
     protected virtual void Interact()

b0603d7641b8ed29e10784ce3c9429816885d21a
AndreiMoraru123 <81184255+AndreiMoraru123@users.noreply.github.com>
2024-12-20 12:23:18 +0200

    more sophisticated path state tracking in editor

--- Assets/Scripts/Enemy/Path.cs
+++ Assets/Scripts/Enemy/Path.cs
@@ -1,19 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEditor;
 using UnityEngine;
 
 public class Path : MonoBehaviour
 {
-    public List<Transform> waypoints = new List<Transform>();
-    // Start is called before the first frame update
-    void Start()
-    {
+    public List<Transform> waypoints;
 
+    [SerializeField]
+    private bool alwaysDrawPath;
+
+    [SerializeField]
+    private bool drawAsLoop;
+
+    [SerializeField]
+    private bool drawNumbers;
+    public Color debugColor = Color.white;
+
+#if UNITY_EDITOR
+    public void OnDrawGizmos()
+    {
+        if (alwaysDrawPath)
+        {
+            DrawPath();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DrawPath()
     {
+        for (int i = 0; i < waypoints.Count; i++)
+        {
+            GUIStyle labelStyle = new GUIStyle();
+            labelStyle.fontSize = 30;
+            labelStyle.normal.textColor = debugColor;
 
+            if (drawNumbers)
+            {
+                Handles.Label(waypoints[i].position, i.ToString(), labelStyle);
+            }
+
+            if (i >= 1)
+            {
+                Gizmos.color = debugColor;
+                Gizmos.DrawLine(waypoints[i - 1].position, waypoints[i].position);
+
+                if (drawAsLoop)
+                {
+                    Gizmos.DrawLine(waypoints[i - 1].position, waypoints[0].position);
+                }
+            }
+        }
     }
+
+    public void OnDrawGizmosSelected()
+    {
+        if (alwaysDrawPath) return;
+        else DrawPath();
+    }
+#endif
 }

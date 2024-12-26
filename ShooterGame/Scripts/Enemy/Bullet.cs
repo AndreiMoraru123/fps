67ea67ccf2531db8155aeb2e9b6dc7821c9b0bbd
AndreiMoraru123 <81184255+AndreiMoraru123@users.noreply.github.com>
2024-12-26 10:12:36 +0200

    bullet effects

--- ShooterGame/Scripts/Enemy/Bullet.cs
+++ ShooterGame/Scripts/Enemy/Bullet.cs
@@ -15,7 +15,21 @@ public class Bullet : MonoBehaviour
         if (hitTransform.CompareTag("Target"))
         {
             Debug.Log("Hit " + hitTransform.gameObject.name);
+            CreateBulletImpactEffect(collision);
             Destroy(gameObject);
         }
+        if (hitTransform.CompareTag("Solid"))
+        {
+            Debug.Log("Hit a wall");
+            CreateBulletImpactEffect(collision);
+            Destroy(gameObject);
+        }
+    }
+
+    private void CreateBulletImpactEffect(Collision collision)
+    {
+        ContactPoint contact = collision.contacts[0];
+        var hole = Instantiate(GlobalReferences.Instance.bulletImpactEffectPrefab, contact.point, Quaternion.LookRotation(contact.normal));
+        hole.transform.SetParent(collision.gameObject.transform);
     }
 }
--- ShooterGame/Scripts/Weapon/PlayerWeapon.cs
+++ ShooterGame/Scripts/Weapon/PlayerWeapon.cs
@@ -1,20 +1,82 @@
-
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerWeapon : WeaponBase
 {
-    protected override void Start()
+    public enum ShootingMode
     {
-        base.Start();
-        bulletVelocity = 50f;
-        fireRate = 0.1f;
+        Single, Burst, Auto
     }
-    public void RapidFire()
+    public ShootingMode currentShootingMode;
+    public Camera playerCamera;
+    public bool isShooting, readyToShoot;
+    private bool allowReset = true;
+    public float shootingDelay = 2f;
+    public int bulletsPerBurst = 3;
+    public int burstBulletsLeft;
+    public float spreadIntensity;
+
+    public void Awake()
     {
-        // TODO: fire with no restrictions
+        readyToShoot = true;
+        burstBulletsLeft = bulletsPerBurst;
+    }
+
+    private void DelayedFire()
+    {
+        readyToShoot = false;
+        Vector3 shootingDirection = CalculateDirectionAndSpread().normalized;
+
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.identity);
+        bullet.transform.forward = shootingDirection;
+
+        bullet.GetComponent<Rigidbody>().AddForce(shootingDirection * bulletVelocity, ForceMode.Impulse);
+        StartCoroutine(DestroyBulletAfterTime(bullet, bulletPrefabLifetime));
+
+        if (allowReset)
+        {
+            Invoke("ResetShot", shootingDelay);
+            allowReset = false;
+        }
+
+        // we already shoot once before this check
+        if (currentShootingMode == ShootingMode.Burst && burstBulletsLeft > 1)
+        {
+            burstBulletsLeft--;
+            Invoke("DelayedFire", shootingDelay);
+        }
+    }
+
+    private void ResetShot()
+    {
+        readyToShoot = true;
+        allowReset = true;
+    }
+
+    public Vector3 CalculateDirectionAndSpread()
+    {
+        Ray ray = playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+        Vector3 targetPoint;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            targetPoint = hit.point;
+        }
+        else
+        {
+            targetPoint = ray.GetPoint(100);
+        }
+
+        Vector3 direction = targetPoint - bulletSpawn.position;
+        float x = Random.Range(-spreadIntensity, spreadIntensity);
+        float y = Random.Range(-spreadIntensity, spreadIntensity);
+
+        return direction + new Vector3(x, y, 0);
+    }
+
+    private void RapidFire()
+    {
+        // Fire with no restrictions
         GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.identity);
         bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward.normalized * bulletVelocity, ForceMode.Impulse);
         StartCoroutine(DestroyBulletAfterTime(bullet, bulletPrefabLifetime));
@@ -22,9 +84,20 @@ public class PlayerWeapon : WeaponBase
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (currentShootingMode == ShootingMode.Auto)
         {
-            Fire(bulletSpawn.forward);
+            // hold the LMB
+            isShooting = Input.GetKey(KeyCode.Mouse0);
+        }
+        else if (currentShootingMode == ShootingMode.Single || currentShootingMode == ShootingMode.Burst)
+        {
+            isShooting = Input.GetKeyDown(KeyCode.Mouse0);
+        }
+
+        if (readyToShoot && isShooting)
+        {
+            burstBulletsLeft = bulletsPerBurst;
+            DelayedFire();
         }
     }
 }

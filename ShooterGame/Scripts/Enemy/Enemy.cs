d06e2a4aac0e75e0d6c0463eb343d6b279c64cc7
AndreiMoraru123 <81184255+AndreiMoraru123@users.noreply.github.com>
2025-01-12 06:39:09 +0200

    nuke old enemy state machine

--- ShooterGame/Scripts/Enemy/Enemy.cs
+++ ShooterGame/Scripts/Enemy/Enemy.cs
@@ -8,12 +8,7 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField]
     protected int HP = 100;
-
-    // TODO: do I use this getter?
-    public int GetHealth { get => HP; }
     protected Animator animator;
-    protected NavMeshAgent agent;
-    public NavMeshAgent Agent { get => agent; }
     public bool isDead;
 
     void Awake()

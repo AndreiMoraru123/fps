e914fe4ea99a64805f687855edaaaef7eb61c968
AndreiMoraru123 <81184255+AndreiMoraru123@users.noreply.github.com>
2025-01-13 10:29:17 +0200

    sync with depthai repo part 2

--- ShooterGame/Scripts/Enemy/XBot/XBotHand.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class XBotHand : MonoBehaviour
-{
-    public float damage;
-}
--- ShooterGame/Scripts/Managers/DeviceManager.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Runtime.InteropServices;
-
-public static class DeviceManager
-{
-    [DllImport("depthai-unity", CallingConvention = CallingConvention.Cdecl)]
-    public static extern IntPtr GetAllDevices();
-
-}
